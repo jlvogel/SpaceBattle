736ec7fea13131677e5a699903c1ebdd8b072d64
jlvogel <jlv153@gmail.com>
2023-11-03 10:41:31 -0400

    Started psuedocode

---
+++ script.js
@@ -0,0 +1,19 @@
+// USS Assembly - attacks first
+
+// 6 Aliens - we attack them one at a time
+
+// if we destroy all 6 aliens we win
+
+// hull, firepower, accuracy
+
+// USS Assembly starts out with:
+// hull = 20
+// firepower = 5
+// accuracy = 7
+
+// Aliens (each one) have following ranged properties
+// (determined randomly)
+
+// (3 <= hull <= 6)
+// (2 <= firepower <= 4)
+// (0.6 <= accuracy <= 0.8)
